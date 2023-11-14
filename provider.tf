3bd4b3c9d23c4fc16ab31bc2ed1a30bd5e4e587d
Jay Vadgama <68870219+jayvadgama@users.noreply.github.com>
2023-11-14 10:11:08 +0300

    Updated provider.tf

--- provider.tf
+++ provider.tf
@@ -1,5 +1,5 @@
 provider "aws" {
-  access_key = "AKIAYC6AM2BPSLEI7PPE"
-  secret_key = "KCxMgXJuoa7u6gnpRZLrLePgjhJxg7OsTPo1uKi5"
-  region     = "us-east-1"
+  access_key = "YOUR_ACCESS_KEY"
+  secret_key = "YOUR_SECRET_KEY"
+  region     = "YOUR_REGION_OF_CHOICE"
 }
